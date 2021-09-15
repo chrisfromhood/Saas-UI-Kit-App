8c4f4d4307cd302a141500afa23597d385b5937d
Ivan Stantic <ivan.stantic@reversinglabs.com>
2021-09-15 13:26:56 +0200

    Fix for the postcss nesting issue

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,9 @@
-const { POSTCSS_MODES } = require("@craco/craco");
-
 module.exports = {
   style: {
     postcss: {
-      mode: POSTCSS_MODES.file,
+      plugins: [
+        require("postcss-nested"),
+      ],
     },
   },
 };
