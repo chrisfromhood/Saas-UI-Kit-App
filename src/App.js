0ec5a76d76db0f57f142ef78efc4300bb988a634
chrisfromhood <sebenjisu@gmail.com>
2021-09-16 15:45:19 +0200

    Sidebar on progress

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import cs from "./App.module.css";
 import userPhoto from "./assets/img/user-icon.jpg";
-
+import dashboardIcon from "./assets/img/icons/dashboardicon.svg";
+import tasksIcon from "./assets/img/icons/tasks-activeicon.svg";
+import emailIcon from "./assets/img/icons/emailicon.svg";
+import contactsIcon from "./assets/img/icons/contactsicon.svg";
+import chatIcon from "./assets/img/icons/chaticon.svg";
+import dealsIcon from "./assets/img/icons/dealsicon.svg";
+// className={cs[""]}
 function App() {
   return (
     <div className={cs["container"]}>
@@ -20,6 +26,37 @@ function App() {
             <p className={cs["user-details_mail"]}>s.ferguson@gmail.com</p>
           </div>
         </div>
+        <nav className={cs["side-options"]}>
+          <div className={cs["side-option_item"]}>
+            <img src={dashboardIcon} alt="File Upload Icon" className={cs["option_item_icon"]} />
+            <span>Dashboard</span>
+          </div>
+
+          <div className={cs["side-option_item"]}>
+            <img src={tasksIcon} alt="File Upload Icon" />
+            <span>Tasks</span>
+          </div>
+
+          <div className={cs["side-option_item"]}>
+            <img src={emailIcon} alt="File Upload Icon" />
+            <span>Email</span>
+          </div>
+
+          <div className={cs["side-option_item"]}>
+            <img src={contactsIcon} alt="File Upload Icon" />
+            <span>Contacts</span>
+          </div>
+
+          <div className={cs["side-option_item"]}>
+            <img src={chatIcon} alt="File Upload Icon" />
+            <span>Chat</span>
+          </div>
+
+          <div className={cs["side-option_item"]}>
+            <img src={dealsIcon} alt="File Upload Icon" />
+            <span>Deals</span>
+          </div>
+        </nav>
       </div>
       <div className={cs["content"]}>CONTENT</div>
     </div>
