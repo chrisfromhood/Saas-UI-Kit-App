a47eba21a354a259f878aa381345c93d862c01e9
chrisfromhood <sebenjisu@gmail.com>
2021-09-19 22:33:35 +0200

    searchbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import cs from "./App.module.css";
 import userPhoto from "./assets/img/user-icon.jpg";
 import dashboardIcon from "./assets/img/icons/dashboardicon.svg";
-import tasksIcon from "./assets/img/icons/tasks-activeicon.svg";
+import tasksIcon from "./assets/img/icons/tasks.svg";
 import emailIcon from "./assets/img/icons/emailicon.svg";
 import contactsIcon from "./assets/img/icons/contactsicon.svg";
 import chatIcon from "./assets/img/icons/chaticon.svg";
 import dealsIcon from "./assets/img/icons/dealsicon.svg";
+import settingsIcon from "./assets/img/icons/settingsicon.svg";
+// SEARCH
+import searchIcon from "./assets/img/icons/search.svg";
+import notifyIcon from "./assets/img/icons/notifications.svg";
+// STATUS ICON
+import activeIcon from "./assets/img/circle/active_circle.svg";
+import completedIcon from "./assets/img/circle/completed_circle.svg";
+import endedIcon from "./assets/img/circle/ended_circle.svg";
+
 // className={cs[""]}
+//className={`${cs["prva-klasa"]} ${cs["druga-klasa"]}`}
 function App() {
   return (
     <div className={cs["container"]}>
-      <div className={cs["searchbar"]}>SEARCHBAR</div>
+      <div className={cs["searchbar"]}>
+        <form action="#" className={cs["search"]}>
+          <button className={cs["search__button"]}>
+            <img src={searchIcon} alt="Search Icon" />
+          </button>
+          <input type="text" className={cs["search__input"]} placeholder="Search tasks" />
+        </form>
+        <div className={cs["notify__icon"]}>
+          <img src={notifyIcon} alt="Notify Icon" />
+        </div>
+      </div>
 
       <div className={cs["sidebar"]}>
         <div className={cs["side-logo"]}>
@@ -28,35 +48,54 @@ function App() {
         </div>
         <nav className={cs["side-options"]}>
           <div className={cs["side-option_item"]}>
-            <img src={dashboardIcon} alt="File Upload Icon" className={cs["option_item_icon"]} />
+            <img src={dashboardIcon} alt="Dashboard Icon" className={cs["option_item_icon"]} />
             <span>Dashboard</span>
           </div>
 
-          <div className={cs["side-option_item"]}>
-            <img src={tasksIcon} alt="File Upload Icon" />
+          <div className={`${cs["side-option_item"]} ${cs["side-option_selected"]}`}>
+            <img src={tasksIcon} alt="Tasks Icon" />
             <span>Tasks</span>
           </div>
 
+          <div className={cs["side-status"]}>
+            <div className={cs["side-status_item"]}>
+              <img src={activeIcon} alt="Active Status Icon" />
+              <span>Active</span>
+            </div>
+            <div className={cs["side-status_item"]}>
+              <img src={completedIcon} alt="Completed Status Icon" />
+              <span>Completed</span>
+            </div>
+            <div className={cs["side-status_item"]}>
+              <img src={endedIcon} alt="Ended Status Icon" />
+              <span>Ended</span>
+            </div>
+          </div>
+
           <div className={cs["side-option_item"]}>
-            <img src={emailIcon} alt="File Upload Icon" />
+            <img src={emailIcon} alt="Email Icon" />
             <span>Email</span>
           </div>
 
           <div className={cs["side-option_item"]}>
-            <img src={contactsIcon} alt="File Upload Icon" />
+            <img src={contactsIcon} alt="Contacts Icon" />
             <span>Contacts</span>
           </div>
 
           <div className={cs["side-option_item"]}>
-            <img src={chatIcon} alt="File Upload Icon" />
+            <img src={chatIcon} alt="Chat Icon" />
             <span>Chat</span>
           </div>
 
           <div className={cs["side-option_item"]}>
-            <img src={dealsIcon} alt="File Upload Icon" />
+            <img src={dealsIcon} alt="Deals Icon" />
             <span>Deals</span>
           </div>
         </nav>
+        <div className={cs["side-option_settings"]}>
+          <img src={settingsIcon} alt="Settings Icon" />
+          <span>Settings</span>
+        </div>
       </div>
       <div className={cs["content"]}>CONTENT</div>
     </div>
