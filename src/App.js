2113d05cef27b0965841a32cdae16f145f2488d1
chrisfromhood <sebenjisu@gmail.com>
2021-09-23 13:51:29 +0200

    Landing page almost done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Searchbar from "./components/Searchbar";
 // HEADER
 import arrowIcon from "./assets/img/icons/arrow.svg";
 import heroPhoto from "./assets/img/sign/undraw_feeling_proud.svg";
+import eyeIcon from "./assets/img/sign/eye.svg";
 
 // className={cs[""]}
 //className={`${cs["prva-klasa"]} ${cs["druga-klasa"]}`}
@@ -13,14 +14,48 @@ function App() {
   return (
     <div>
       <section className={cs["landing-page"]}>
-        <div className={cs["sign-in"]}>SIGN IN</div>
+
+        <div className={cs["sign-in"]}>
+          <div className={cs["sign-in_wrapper"]}>
+
+            <div className={cs["side-logo"]}>
+              <span>SaaS Kit</span>
+            </div>
+
+            <h1 className={cs["sign-in_header"]}>Sign In</h1>
+
+            <input className={cs["email"]} type="text" placeholder="Email"></input>
+
+            <div className={cs["password_input"]}>
+              <input className={cs["password"]} type="text" placeholder="Password"></input>
+              <img src={eyeIcon} alt="Eye" className={cs["eye_icon"]} />
+            </div>
+
+            <div className={cs["checkbox_wrapper"]}>
+              <input className={cs["checkbox_icon"]} type="checkbox" />
+              <span>Keep me logged in</span>
+            </div>
+
+            <div className={cs["btn-wrapper"]}>
+              <button className={cs["btn-sign_in"]}> Sign in</button>
+            </div>
+
+            <div className={cs["text-wrapper"]}>
+              <p className={cs["text-sign_in"]}> Don't have an account?
+                <span>Sign up</span></p>
+            </div>
+
+          </div>
+
+        </div>{/* -------End of .sign-in---- */}
+
         <div className={cs["big-hero"]}>
           <div className={cs["big-hero_wrapper"]}>
             <img src={heroPhoto} alt="Hero" className={cs["big-hero_photo"]} />
             <h1 className={cs["big-hero_header"]}>Manage everything in one place</h1>
             <p className={cs["big-hero_text"]}>Accusam noluisse mel et. Ius duis menandri ne, rebum exerci ad his. Quo at tollit veniam assueverit.</p>
           </div>
-        </div>
+        </div> {/* -------End of .big-hero---- */}
 
 
       </section>
